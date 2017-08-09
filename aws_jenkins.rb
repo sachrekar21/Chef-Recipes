1152a8ead0508a923b2b8e01a6d21628cd995be0
sachrekar21 <sachrekar@cigital.com>
2017-08-09 10:17:33 -0400

    Update aws_jenkins.rb

--- aws_jenkins.rb
+++ aws_jenkins.rb
@@ -6,14 +6,14 @@ machine_batch do
     machine_options bootstrap_options: {
     key_name: 'pc-jenkins-key',
     key_path: '/home/ubuntu/.ssh',
-    image_id: "ami-6c376e17", # Image with Jenkins pre installed
-    instance_type: "t2.micro",
+    image_id: "ami-65725b1e", # Image with Jenkins pre installed
+    instance_type: "t2.medium",
     network_interfaces: [
     {
       associate_public_ip_address: true,
       device_index: 0,
-      subnet_id: "subnet-3fee8e13",
-      groups: ["sg-bef3f1cf"] }
+      subnet_id: "PUT_YOUR_SUBNET_ID HERE",
+      groups: ["PUT_YOUR_SG_ID_HERE"] }
       ],
  }
  end
